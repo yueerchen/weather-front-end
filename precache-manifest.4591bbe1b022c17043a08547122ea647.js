7d4c32a58cbd8cc921b43c39208ac8d34878f028
Yueer <yueer.chen0318@gmail.com>
2019-09-02 21:37:48 +1000

    Updates

---
+++ precache-manifest.4591bbe1b022c17043a08547122ea647.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0d4d47388465bc196166d132872e916",
+    "url": "/weather-front-end/index.html"
+  },
+  {
+    "revision": "ef61462c9ee72099acd8",
+    "url": "/weather-front-end/static/css/main.6d0e3f1a.chunk.css"
+  },
+  {
+    "revision": "a1396de3a2958f6c0b3b",
+    "url": "/weather-front-end/static/js/2.c32b7f1c.chunk.js"
+  },
+  {
+    "revision": "ef61462c9ee72099acd8",
+    "url": "/weather-front-end/static/js/main.16af8bba.chunk.js"
+  },
+  {
+    "revision": "d0b5277eabfb6754d505",
+    "url": "/weather-front-end/static/js/runtime~main.9b82df7a.js"
+  }
+]);
